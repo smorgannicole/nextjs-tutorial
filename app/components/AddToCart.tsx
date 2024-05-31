8e1c45b72b950bd20a4fcb4e2dc7b6e8e8e5c2ff
smorgannicole <morgann.smithh@yahoo.com>
2024-05-31 11:16:05 -0700

    client vs server components

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const AddToCart = () => {
+  return (
+    <div>
+      <button onClick={() => console.log("click")}>Add To Cart</button>
+    </div>
+  );
+};
+
+export default AddToCart;
