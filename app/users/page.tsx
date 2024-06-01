e277a99b9adf7b96637a8cdc9aeebdc74453e342
smorgannicole <morgann.smithh@yahoo.com>
2024-06-01 07:51:59 -0700

    data cache

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,5 +1,5 @@
 import { link } from "fs";
-import React from "react";
+import React, { cache } from "react";
 
 interface User {
   id: number;
@@ -7,7 +7,9 @@ interface User {
 }
 
 const UsersPage = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    next: { revalidate: 10 },
+  });
   const users: User[] = await res.json();
 
   return (
