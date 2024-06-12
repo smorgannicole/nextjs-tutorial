ab8f9721010acc7b940d35946a2cfe1352277c69
Morgan <smorgannicole@gmail.com>
2024-06-12 10:07:40 -0500

    navigation vs programmatic navigation

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,6 +5,12 @@ const NavBar = () => {
   return (
     <div className="flex bg-slate-200 p-5 gap-5">
       <Link href="/">Next.js</Link>
+      {/* Link only downloads the content of the target page */}
+      {/* Link also pre-fetches the links that are in the viewport... */}
+      {/* ie on Users page, next.js will pre-fetch sortOrder for name and email to enhance performance time */}
+      {/* when navigating in app, next.js stores payload of pages on client thru caching... */}
+      {/* so, when navigating to another page, next.js wont make a request to backend again, it will reference client cache... */}
+      {/* caches only exists for one session and clears after full page reload */}
       <Link href="/users">Users</Link>
     </div>
   );
--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+// import { useRouter } from "next/router";
+// ^^ won't work bc it's part of old pages router and it won't mount
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const NewUserPage = () => {
-  return <div>NewUserPage</div>;
+  const router = useRouter();
+  return (
+    <button className="btn btn-primary" onClick={() => router.push("/users")}>
+      Create
+    </button>
+  );
+  // in anon fxn in onClick we can't use Link, so we must use programmatic navigation ie Router obj
 };
 
 export default NewUserPage;
