c76bb254dd005705ce7da8bd539cd483b6066b70
Morgan <smorgannicole@gmail.com>
2024-06-25 18:13:25 -0500

    optimizing images

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,34 @@
-import Link from "next/link";
-import ProductCard from "./components/ProductCard";
+// import Link from "next/link";
+// import ProductCard from "./components/ProductCard";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+import Image from "next/image";
+import seattlePark from "@/public/images/seattle-natural-park.jpg";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
   // getServerSession fxn from next auth retrieves current user session on server side
 
   return (
-    <main>
-      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
+    <main className="relative h-screen">
+      {/* <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
       <Link href="/users">Users</Link>
-      <ProductCard />
+      <ProductCard /> */}
+      {/* <Image
+        src={seattlePark}
+        alt="a sea of small yellow flowers amidst vibrant green foliage"
+      /> */}
+      {/* this component is built on top of the standard img element in html, but... */}
+      {/* under the hood, it auto-compresses and resizes the image based on device size */}
+      <Image
+        src="https://bit.ly/react-cover"
+        alt="react logo over pink and teal diagonal gradient"
+        fill
+        className="object-cover"
+      />
+      {/* for this to work, this domain needs to be registered in config file */}
+      {/* with remote imgs, dimensions should always be provided bc next.js doesn't know their size */}
+      {/* fill makes img take entire screen (may stretch img) */}
     </main>
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "bit.ly",
+        // for security reasons, should aim to be as specific as possible here
+      },
+    ],
+    // this is where we add 1 or more domains where we're allowed to serve images...
+    // this is for security reasons otherwise anyone could take advantage of the endpoint that...
+    // next.js exposes for seerving optimized images
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
